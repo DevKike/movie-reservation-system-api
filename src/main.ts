dff9d0471e479995f163cf3ec001103128a939b7
DevKike <alvaronarvaez151@gmail.com>
2025-03-27 14:56:34 -0500

    Feat: enhance server bootstrap logging and configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  Logger.log(
+    `Server running on mode ${configService.get<string>('NODE_ENV')}`,
+    'Bootstrap',
+  );
+
+  await app.listen(configService.get<number>('SERVER_PORT') || 3000);
 }
-bootstrap();
+
+void bootstrap();
