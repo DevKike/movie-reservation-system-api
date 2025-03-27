80fc784cbd470cec820e97149dc345af84a0c766
DevKike <alvaronarvaez151@gmail.com>
2025-03-27 14:56:22 -0500

    Feat: add environment configuration and database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './infrastructure/database/database.module';
+import { validationSchema } from './infrastructure/environments/env-validation.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      validationSchema,
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
