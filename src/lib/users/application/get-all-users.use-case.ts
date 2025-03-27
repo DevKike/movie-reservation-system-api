b71c1ad2318ccb553c8280e91d00112618dc99d5
DevKike <alvaronarvaez151@gmail.com>
2025-03-27 16:51:14 -0500

    Feat: implement GetAllUsers use case for retrieving all users

---
+++ src/lib/users/application/get-all-users.use-case.ts
@@ -0,0 +1,11 @@
+import { IUser } from '../domain/interfaces/entity/users.entity.interface';
+import { IUsersUseCase } from '../domain/interfaces/use-case/users.use-case.interface';
+import { IUserService } from '../domain/services/users.service.interface';
+
+export class GetAllUsersUseCase implements IUsersUseCase<IUser[]> {
+  constructor(private readonly _usersService: IUserService) {}
+
+  async execute(): Promise<IUser[]> {
+    return await this._usersService.getAll();
+  }
+}
