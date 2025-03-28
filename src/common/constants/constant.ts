1dca5c4352b87a4db962d54b1c3e7875344eac4a
DevKike <alvaronarvaez151@gmail.com>
2025-03-28 01:19:29 -0500

    Feat: add ROLE_SERVICE constant and GET_ALL_ROLES use case to improve role management

--- src/common/constants/constant.ts
+++ src/common/constants/constant.ts
@@ -2,6 +2,9 @@ export const CONSTANT = {
   PROVIDERS: {
     //USERS
     USER_SERVICE: 'UserService',
+
+    //ROLES
+    ROLE_SERVICE: 'RoleService',
   },
 
   USE_CASES: {
@@ -9,5 +12,8 @@ export const CONSTANT = {
     GET_ALL_USERS: 'GetAllUsersUseCase',
     GET_USER: 'GetUserUseCase',
     UPDATE_USER: 'UpdateUserUseCase',
+
+    //ROLES
+    GET_ALL_ROLES: 'GetAllRolesUseCase',
   },
 };
