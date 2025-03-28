6de821c2c3903d2ba683607bdab7e717cbea8711
DevKike <alvaronarvaez151@gmail.com>
2025-03-28 02:03:44 -0500

    Refactor: remove unused user seed messages from seed-messages constant

--- src/common/constants/seed-messages.constant.ts
+++ src/common/constants/seed-messages.constant.ts
@@ -4,9 +4,4 @@ export const SEED_MESSAGES = {
     ALREADY_COMPLETED: 'Role seed already completed',
     ERROR: 'Error seeding roles',
   },
-  USERS: {
-    COMPLETED: 'User seed completed',
-    ALREADY_COMPLETED: 'User seed already completed',
-    ERROR: 'Error seeding users',
-  },
 };
