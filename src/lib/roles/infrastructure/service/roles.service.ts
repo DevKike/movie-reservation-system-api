3822727ee99a3c3b2cabcaba108e3f440ab49a9f
DevKike <alvaronarvaez151@gmail.com>
2025-03-29 16:16:48 -0500

    Refactor: rename IRoleService to IRolesService and implement get method for role retrieval

--- src/lib/roles/infrastructure/service/roles.service.ts
+++ src/lib/roles/infrastructure/service/roles.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { IRoleService } from '../../domain/interfaces/service/roles.service.interface';
+import { IRolesService } from '../../domain/interfaces/service/roles.service.interface';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role } from '../entity/roles.entity';
 import { Repository } from 'typeorm';
 import { IRole } from '../../domain/interfaces/entity/roles.entity.interface';
 import { NotFoundException } from 'src/lib/common/domain/exceptions/not-found.exception';
+import { ROLES } from '../../domain/enums/roles.enum';
 
 @Injectable()
-export class RolesService implements IRoleService {
+export class RolesService implements IRolesService {
   constructor(
     @InjectRepository(Role) private readonly _roleRepository: Repository<Role>,
   ) {}
@@ -18,5 +19,13 @@ export class RolesService implements IRoleService {
     if (!roles.length) throw new NotFoundException('Roles were not found');
 
     return roles;
+  }
+
+  async get(name: ROLES): Promise<IRole> {
+    const role = await this._roleRepository.findOne({ where: { name: name } });
+
+    if (!role) throw new NotFoundException('Role was not found');
+
+    return role;
   }
 }
