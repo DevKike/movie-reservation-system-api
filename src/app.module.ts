ecd1290f0a3b0cbcdad12bb1e23bc40107d83968
DevKike <alvaronarvaez151@gmail.com>
2025-03-29 18:26:42 -0500

    Refactor: move seeders to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './lib/users/users.module';
 import { CoreModule } from './core/core.module';
 import { RolesModule } from './lib/roles/roles.module';
-import { SeederModule } from './seeds/seeder.module';
+import { SeederModule } from './core/database/seeds/seeder.module';
 import { AuthModule } from './lib/auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 
