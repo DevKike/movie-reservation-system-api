59b9f6c440f8c2e33c79908c5a42a5fa5162ca97
DevKike <alvaronarvaez151@gmail.com>
2025-03-30 14:36:48 -0500

    feat: enhance JwtProvider to utilize environment configuration for token signing and verification

--- src/shared/services/jwt/jwt.service.ts
+++ src/shared/services/jwt/jwt.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { IJwtPayload } from 'src/lib/common/domain/services/interfaces/jwt/jwt-payload.interface';
 import { IJwtService } from 'src/lib/common/domain/services/interfaces/jwt/jwt.service.interface';
 
 @Injectable()
 export class JwtProvider implements IJwtService {
-  constructor(private readonly _jwtService: JwtService) {}
+  constructor(
+    private readonly _jwtService: JwtService,
+    private readonly _configService: ConfigService,
+  ) {}
 
   async signToken(payload: IJwtPayload): Promise<string> {
-    return await this._jwtService.signAsync(payload);
+    return await this._jwtService.signAsync(payload, {
+      secret: this._configService.get<string>('JWT_SECRET_KEY'),
+      expiresIn: this._configService.get<string>('JWT_EXPIRES_IN'),
+    });
   }
 
   async verifyToken(token: string): Promise<IJwtPayload> {
-    return await this._jwtService.verifyAsync(token);
+    return await this._jwtService.verifyAsync(token, {
+      secret: this._configService.get<string>('JWT_SECRET_KEY'),
+    });
   }
 }
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { HashService } from './services/hash/hash.service';
 import { CONSTANT } from 'src/common/constants/constant';
 import { JwtProvider } from './services/jwt/jwt.service';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
 
 const providers = [
   { provide: CONSTANT.PROVIDERS.HASH_SERVICE, useClass: HashService },
@@ -11,14 +10,7 @@ const providers = [
 ];
 
 @Module({
-  imports: [
-    JwtModule.registerAsync({
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET_KEY'),
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [JwtModule],
   providers: [...providers],
   exports: [...providers.map((provide) => provide.provide)],
 })
