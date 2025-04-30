3b8fcac169905459c26b0e7ffadde916eb787b06
DevKike <alvaronarvaez.dev@gmail.com>
2025-04-30 17:47:53 -0500

    Refactor: remove express Request extension from IRequest interface for simplicity

--- src/lib/auth/infrastructure/interfaces/request/auth-request.interface.ts
+++ src/lib/auth/infrastructure/interfaces/request/auth-request.interface.ts
@@ -1,6 +1,5 @@
-import { Request } from 'express';
 import { IJwtPayload } from 'src/lib/common/domain/providers/interfaces/jwt/jwt-payload.interface';
 
-export interface IRequest extends Request {
+export interface IRequest {
   user: IJwtPayload;
 }
