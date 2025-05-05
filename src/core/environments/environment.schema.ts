ac0751ced37baea42f7ef22eaace1b2c2836fea0
DevKike <alvaronarvaez.dev@gmail.com>
2025-05-04 19:40:21 -0500

    Feat: restore DB_SYNCHRONIZE property in environment schema for database synchronization configuration

--- src/core/environments/environment.schema.ts
+++ src/core/environments/environment.schema.ts
@@ -12,6 +12,7 @@ export const environmentSchema = Joi.object({
   DB_USERNAME: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
   DB_NAME: Joi.string().required(),
+  DB_SYNCHRONIZE: Joi.boolean().default(false),
 
   HASH_SALT_ROUNDS: Joi.number().default(10),
 
