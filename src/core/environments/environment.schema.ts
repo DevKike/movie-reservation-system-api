066f2b5dcbadf301a6a717045bdccd53657f3d59
DevKike <alvaronarvaez.dev@gmail.com>
2025-05-13 14:14:46 -0500

    refactor: consolidate JWT keys and expiration fields for consistency in environment schema

--- src/core/environments/environment.schema.ts
+++ src/core/environments/environment.schema.ts
@@ -16,10 +16,9 @@ export const environmentSchema = Joi.object({
 
   HASH_SALT_ROUNDS: Joi.number().default(10),
 
-  JWT_ACCESS_SECRET_KEY: Joi.string().required(),
-  JWT_ACCESS_EXPIRES_AT: Joi.string().default('1h'),
-  JWT_REFRESH_SECRET_KEY: Joi.string().required(),
-  JWT_REFRESH_EXPIRES_AT: Joi.string().default('7d'),
+  JWT_SECRET_KEY: Joi.string().required(),
+  JWT_ACCESS_EXPIRES_IN: Joi.string().default('1h'),
+  JWT_REFRESH_EXPIRES_IN: Joi.string().default('7d'),
   JWT_AUDIENCE: Joi.string().default('my-api'),
   JWT_ISSUER: Joi.string().default('my-api'),
 
