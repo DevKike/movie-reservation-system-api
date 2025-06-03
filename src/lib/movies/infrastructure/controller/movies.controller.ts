bf053dc7d326d9427ecbdefc8e9106ba0c7098c2
DevKike <alvaronarvaez.dev@gmail.com>
2025-06-02 21:51:54 -0500

    fix: update parameter name from posterFile to posterImage in addMovie method

--- src/lib/movies/infrastructure/controller/movies.controller.ts
+++ src/lib/movies/infrastructure/controller/movies.controller.ts
@@ -7,10 +7,12 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { CONSTANT } from 'src/common/constants/constant';
-import { IAuth } from 'src/lib/auth/domain/interfaces/entity/auth.entity.interface';
 import { IUseCase } from 'src/lib/common/domain/use-case/interfaces/use-case.interface';
 import { AddMovieDTO } from '../dtos/add-movie.dto';
-import { IAddMovieData } from '../../domain/interfaces/entity/movies.entity.interface';
+import {
+  IAddMovieData,
+  IMovie,
+} from '../../domain/interfaces/entity/movies.entity.interface';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { IUploadFile } from 'src/lib/common/domain/providers/interfaces/uploads/upload-file.interface';
 import { ActiveUser } from 'src/lib/auth/infrastructure/decorators/auth/active-user.decorator';
@@ -20,7 +22,7 @@ import { IUser } from 'src/lib/users/domain/interfaces/entity/users.entity.inter
 export class MoviesController {
   constructor(
     @Inject(CONSTANT.USE_CASES.MOVIE.ADD_MOVIE)
-    private readonly _addMovieUseCase: IUseCase<IAddMovieData, IAuth>,
+    private readonly _addMovieUseCase: IUseCase<IAddMovieData, IMovie>,
   ) {}
 
   @UseInterceptors(FileInterceptor(CONSTANT.KEYS.FILE))
@@ -28,12 +30,12 @@ export class MoviesController {
   async addMovie(
     @ActiveUser('userId') userId: IUser['id'],
     @Body() data: AddMovieDTO,
-    @UploadedFile() posterFile: Express.Multer.File,
-  ): Promise<IAuth> {
+    @UploadedFile() posterImage: Express.Multer.File,
+  ): Promise<IMovie> {
     return await this._addMovieUseCase.execute({
       ...data,
       userId,
-      posterFile: posterFile as IUploadFile,
+      posterImage: posterImage as IUploadFile,
     });
   }
 }
